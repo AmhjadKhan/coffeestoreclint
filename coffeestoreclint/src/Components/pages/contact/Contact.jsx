157a470005bc7308b92534df538cbccd009655c1
Amhjadkhan <amjadpogramer@gmail.com>
2024-06-04 12:51:51 -0500

    this is router added commit

---
+++ coffeestoreclint/src/Components/pages/contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>This is contact pages</h1>
+    </div>
+  )
+}
+
+export default Contact
--- coffeestoreclint/src/routeer/Routeer.jsx
+++ coffeestoreclint/src/routeer/Routeer.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainlaout from "../Components/Mainlayout/Mainlaout";
 import Home from "../Components/pages/Home/Home";
+import About from "../Components/pages/About/About";
+import Contact from "../Components/pages/contact/Contact";
 
 const myCreateRoute = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ const myCreateRoute = createBrowserRouter([
             {
                 path:'/',
                 element:<Home />
+            },
+            {
+                path:"/about",
+                element:<About />
+            },
+            {
+                path:"/contact",
+                element:<Contact />
             }
         ]
     }
