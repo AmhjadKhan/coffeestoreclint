783142a2a6f1456466a1329b08acfffe7f9edee8
Amhjadkhan <amjadpogramer@gmail.com>
2024-06-05 12:08:13 -0500

    this is contact section editing

--- coffeestoreclint/src/Components/pages/contact/Contact.jsx
+++ coffeestoreclint/src/Components/pages/contact/Contact.jsx
@@ -1,9 +1,12 @@
 import Contact1 from "./contact1/Contact1";
+import Contact2 from "./contact2/Contact2";
 
 const Contact = () => {
   return (
     <div>
       <Contact1 />
+      <hr className="mt-6" />
+      <Contact2 />
     </div>
   );
 };
--- coffeestoreclint/src/Components/pages/contact/contact2/Contact2.jsx
+++ coffeestoreclint/src/Components/pages/contact/contact2/Contact2.jsx
@@ -1,40 +1,39 @@
 import { CiLocationOn } from "react-icons/ci";
 import { FaArrowRight } from "react-icons/fa";
 
-
-const Contact1 = () => {
+const Contact2 = () => {
   return (
     <div>
-      <div className="flex flex-col w-full lg:flex-row mt-4">
-        <div className="grid flex-grow h-42 card bg-red-300 rounded-box place-items-center">
+      <div className="flex flex-col w-full lg:flex-row mt-8 mb-4">
+        <div className="grid flex-grow h-42 card bg-base-300 rounded-box place-items-center">
           <h1 className="text-2xl text-gray-900 font-extrabold">Main Brance</h1>
           <div1 className="flex">
             <h1 className="text-xl font-bold">ADDRESS:-</h1>
-            <h1 className="text-xl">Al-wasl-001</h1>
+            <h1 className="text-xl">Qatar t building</h1>
           </div1>
-          <h1 className="text-xl">Al-mina road</h1>
+          <h1 className="text-xl">Sheikhtamim road</h1>
           <div className="flex">
             <h1 className="text-2xl">
               <CiLocationOn />
             </h1>
-            <h1 className="text-xl font-bold">Bur-Dubai, Dubai</h1>
+            <h1 className="text-xl font-bold">Doha, Qatar</h1>
           </div>
           <h1 className="text-xl font-bold">Phone:- 00355224455</h1>
         </div>
         <div className="divider lg:divider-horizontal text-4xl">
           <FaArrowRight />
         </div>
-        <div className="grid flex-grow h-42 card bg-base-300 rounded-box place-items-center">
+        <div className="grid flex-grow h-42 card bg-red-300 rounded-box place-items-center">
           <div className="flex">
             <h1 className="text-xl font-bold">ADDRESS:-</h1>
-            <h1 className="text-xl">3rd floor S building</h1>
+            <h1 className="text-xl">4rd floor kira building</h1>
           </div>
-          <h1 className="text-xl">Sheikhzayed-road</h1>
+          <h1 className="text-xl">Sheikhmuzib road</h1>
           <div className="flex">
             <h1 className="text-2xl">
               <CiLocationOn />
             </h1>
-            <h1 className="text-xl font-bold">Downtown, Dubai</h1>
+            <h1 className="text-xl font-bold">Dhaka,Bangladesh</h1>
           </div>
           <h1 className="text-xl font-bold">Phone:- 00355224455</h1>
         </div>
@@ -43,4 +42,4 @@ const Contact1 = () => {
   )
 }
 
-export default Contact1
+export default Contact2
